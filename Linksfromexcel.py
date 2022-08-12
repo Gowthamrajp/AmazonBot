b0b59d77050acf28548852449090cbf0dfe624dd
Adarsh <adarshp1410@gmail.com>
2022-08-12 18:27:22 +0530

    Added csv file!

--- Linksfromexcel.py
+++ Linksfromexcel.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv ("F:\AmazonBot\Links.csv")   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-print(df["Product Link"][1])
+df = pd.read_csv ("F:\AmazonBot\Links.csv")
+print(df["ProductName"][1])
