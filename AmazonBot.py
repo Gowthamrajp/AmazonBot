99375ef8436d1a9355185a5a6261f1979962aa9e
Gowtham Raj <gowthrj@amazon.com>
2022-08-12 20:04:32 +0530

    Added messaging functionality

--- AmazonBot.py
+++ AmazonBot.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import pywhatkit
 
 #importing links from CSV using pandas
 productData = pd.read_csv ("productData.csv")  
@@ -11,7 +12,7 @@ headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/53
         }
 
         
-def scrapper(name,url):
+def scrapper(name,url,i):
         #Scrapping the Website data into soup
         page = requests.get(url, headers=headers)
         soup = BeautifulSoup(page.content,'html.parser')
@@ -23,12 +24,18 @@ def scrapper(name,url):
         rawPrice=soup.find_all("span", class_="a-price-whole")[0].get_text()
         cleanPrice = ''.join((x for x in rawPrice if x.isdigit()))
         prodPrice=(float(cleanPrice))
-
+        #checking price change
+        if productData["lastKnownPrice"][i]>prodPrice: 
+        #updating price into CSV 
+                productData["lastKnownPrice"][i]=prodPrice
+                pywhatkit.sendwhatmsg_instantly("+919500133791", "Price of "+ name +" changed")
         print(name)
         print(prodName)
         print(prodPrice)
+        productData.to_csv("productData.csv", index=False)
 
 #scrapper function call for each product
 for x in range(0,len(productData["ProductName"])):
-        print(x)
-        scrapper(productData["ProductName"][x], productData["Product Link"][x])
+        print(x+1)
+        scrapper(productData["ProductName"][x], productData["Product Link"][x],x)
+
