3f5a25a89dd19a4e2e297e7aea4d3122b915f6fe
Adarsh <adarshp1410@gmail.com>
2022-08-12 23:08:35 +0530

    Added a text file to enter the whatsapp contact to send the message and the bot scripts can automattically switch between personal and group

--- AmazonBot.py
+++ AmazonBot.py
@@ -7,6 +7,13 @@ import pywhatkit
 #importing links from CSV using pandas
 productData = pd.read_csv (os.getcwd()+"\productData.csv")  
 
+with open(os.getcwd()+"\WhatsAppDetails.txt") as f:
+    contents = f.readline()
+    content = contents[0]
+if content == "+":
+        flag=0
+else:
+        flag=1
 
 headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
         'Accept-Language': 'en-US, en;q=0.5'
@@ -30,7 +37,11 @@ def scrapper(name,url,i):
         if productData["lastKnownPrice"][i]>prodPrice: 
         #updating price into CSV 
                 productData["lastKnownPrice"][i]=prodPrice
-                pywhatkit.sendwhatmsg_to_group_instantly("FaW8GSu5e7TKyvp4RCm6zX",  "Price of "+ name +" Reduced to: "+ str(prodPrice))#+" url is "+url)
+                if(flag == 1):
+                        pywhatkit.sendwhatmsg_to_group_instantly("FaW8GSu5e7TKyvp4RCm6zX",  "Price of "+ name +" Reduced to: "+ str(prodPrice))#+" url is "+url)
+                else:
+                        pywhatkit.sendwhatmsg_instantly(content , "Price of "+ name +" Reduced to: "+ str(prodPrice))#+" url is "+url))
+
         print(name)
         print(prodName)
         print(prodPrice)
